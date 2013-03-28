a794bb1e813d5b471326de7d79999cca89bc3ff2
Mike Bajur <mbajur@gmail.com>
2013-03-28 11:52:44 +0100

    Initial commit

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -1,6 +1,39 @@
 class TodoList
 
-  # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    if items.nil?
+      raise IllegalArgument
+    end
+    @list = items
+    @completed = []
+  end
+
+  def empty?
+    @list.empty?
+  end
+
+  def size
+    @list.size
+  end
+
+  def << (item)
+    @list << item
+    @list
+  end
+
+  def last
+    @list.last
+  end
+
+  def completed? (item)
+    @list.include? [item, true]
+  end
+
+  def first
+    @list.first
+  end
+
+  def complete(index)
+    @completed[index] = true
   end
 end
